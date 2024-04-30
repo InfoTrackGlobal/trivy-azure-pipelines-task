fcaea75e0069f84f069d9f80eb07b86423a610d6
Cameron Gilkeson <46800453+cgilkeson-infotrack@users.noreply.github.com>
2024-04-30 11:23:30 +1000

    Revert "Feature/remove vulnerabilities tab + small ui spacing fix (#11)"
    This reverts commit c7bfa205f11e037d26d6727a0462b6fa0b232688.

--- ui/src/BaseReport.tsx
+++ ui/src/BaseReport.tsx
@@ -3,9 +3,11 @@ import {
     AssuranceReport,
     countReportMisconfigurations,
     countReportSecrets,
+    countReportVulnerabilities,
     Report
 } from './trivy';
 import { SecretsTable } from "./SecretsTable";
+import { VulnerabilitiesTable } from "./VulnerabilitiesTable";
 import { MisconfigurationsTable } from "./MisconfigurationsTable";
 import { Tab, TabBar, TabSize } from "azure-devops-ui/Tabs";
 import { AssuranceTable } from "./AssuranceTable";
@@ -72,6 +74,8 @@ export class BaseReport extends React.Component<BaseReportProps, BaseReportState
                     >
                         <Tab id="secrets" name="Secrets" key="secrets"
                             badgeCount={countReportSecrets(this.props.report)} />
+                        <Tab id="vulnerabilities" name="Vulnerabilities" key="vulnerabilities"
+                            badgeCount={countReportVulnerabilities(this.props.report)} />
                         <Tab id="misconfigurations" name="Misconfigurations" key="misconfigurations"
                             badgeCount={countReportMisconfigurations(this.props.report)} />
                         {
@@ -85,6 +89,12 @@ export class BaseReport extends React.Component<BaseReportProps, BaseReportState
                         this.state.selectedTabId === "secrets" &&
                         <div className="flex-grow">
                             <SecretsTable results={this.props.report.Results} defaultBranch={this.props.report.DefaultBranch} artifactName={this.props.report.ArtifactName} />
+                        </div>
+                    }
+                    {
+                        this.state.selectedTabId === "vulnerabilities" &&
+                        <div className="flex-grow">
+                            <VulnerabilitiesTable results={this.props.report.Results} />
                         </div>
                     }
                     {
--- ui/src/ReportStats.tsx
+++ ui/src/ReportStats.tsx
@@ -3,6 +3,7 @@ import {
     countReportIssues,
     countReportMisconfigurations,
     countReportSecrets,
+    countReportVulnerabilities,
     Report
 } from './trivy';
 import {Card} from "azure-devops-ui/Card";
@@ -29,11 +30,14 @@ export class ReportStats extends React.Component<ReportStatsProps> {
             {
                 name: "Target",
                 value: this.props.report.ArtifactName,
-                width: '220px'
             },
             {
                 name: "Total Issues",
                 value: countReportIssues(this.props.report)
+            },
+            {
+                name: "Vulnerabilities",
+                value: countReportVulnerabilities(this.props.report)
             },
             {
                 name: "Misconfigurations",
@@ -48,7 +52,7 @@ export class ReportStats extends React.Component<ReportStatsProps> {
                 <Card className="flex-grow">
                     <div className="flex-row" style={{flexWrap: "wrap"}}>
                         {stats.map((items, index) => (
-                            <div className="flex-column" style={{minWidth: "120px", width: items.width}} key={index}>
+                            <div className="flex-column" style={{minWidth: "120px"}} key={index}>
                                 <div className="body-m secondary-text">{items.name}</div>
                                 <div className="body-m primary-text">{items.value}</div>
                             </div>
